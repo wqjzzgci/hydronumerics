8570f99d9c7d4d55039fa14fbd4d8cd49986f087
Jan.B.Gregersen@GMail.com <Jan.B.Gregersen@GMail.com>
2010-03-19 13:17:13 +0000

    Added copyright statement at the top of each file

--- HydroNumerics/OpenMI/SDK/Backbone/Quantity.cs
+++ HydroNumerics/OpenMI/SDK/Backbone/Quantity.cs
@@ -1,6 +1,6 @@
 #region Copyright
 /*
-* Copyright (c) 2005,2006,2007, HydroInform
+* Copyright (c) 2010, Jan Gregersen (HydroInform) & Jacob Gudbjerg
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -10,14 +10,14 @@
 *     * Redistributions in binary form must reproduce the above copyright
 *       notice, this list of conditions and the following disclaimer in the
 *       documentation and/or other materials provided with the distribution.
-*     * Neither the name of the HydroInform nor the
+*     * Neither the names of Jan Gregersen (HydroInform) & Jacob Gudbjerg nor the
 *       names of its contributors may be used to endorse or promote products
 *       derived from this software without specific prior written permission.
 *
-* THIS SOFTWARE IS PROVIDED BY "HydroInform" ``AS IS'' AND ANY
+* THIS SOFTWARE IS PROVIDED BY "Jan Gregersen (HydroInform) & Jacob Gudbjerg" ``AS IS'' AND ANY
 * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL "HydroInform" BE LIABLE FOR ANY
+* DISCLAIMED. IN NO EVENT SHALL "Jan Gregersen (HydroInform) & Jacob Gudbjerg" BE LIABLE FOR ANY
 * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -25,7 +25,7 @@
 * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#endregion 
+#endregion
 
 using System;
 using System.Xml;
