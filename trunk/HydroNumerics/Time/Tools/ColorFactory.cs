d7a87e8194a4935b6b47078693405d1155bed08d
Jan.B.Gregersen@GMail.com <Jan.B.Gregersen@GMail.com@9a3f36a8-2785-11df-8e46-3928208ddfa0>
2010-09-08 15:01:59 +0000

    Now curves for different items are shown in different colors

---
+++ trunk/HydroNumerics/Time/Tools/ColorFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace HydroNumerics.Time.Tools
+{
+    public class ColorFactory
+    {
+        Color[] colors;
+        int currentColorIndex;
+
+        public ColorFactory()
+        {
+            colors = new Color[] { Color.Blue, Color.DarkGreen, Color.DarkViolet };
+            
+            currentColorIndex = colors.Length;
+        }
+
+        public ColorFactory(Color[] colors)
+        {
+            this.colors = colors;
+            currentColorIndex = colors.Length;
+        }
+        /// <summary>
+        /// returns a color that is different from the color that was returned the last time this metod was invoked.
+        /// </summary>
+        /// <returns></returns>
+        public Color GetNextColor()
+        {
+            if (currentColorIndex >= colors.Length)
+            {
+                currentColorIndex = 0;
+            }
+            else
+            {
+                currentColorIndex++;
+            }
+            return colors[currentColorIndex];
+        }
+    }
+}
