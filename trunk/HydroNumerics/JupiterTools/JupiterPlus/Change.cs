d4c58b9215a0f84e2f7bbee0b46b7170e4b5d405
jacobgudbjerg@gmail.com <jacobgudbjerg@gmail.com@9a3f36a8-2785-11df-8e46-3928208ddfa0>
2010-10-13 14:04:01 +0000

    New project to handle changes in Jupiter

---
+++ trunk/HydroNumerics/JupiterTools/JupiterPlus/Change.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+
+namespace HydroNumerics.JupiterTools.JupiterPlus
+{
+  public class Change<T>
+  {
+    public string User { get; set; }
+    public string Project { get; set; }
+    public DateTime Date { get; set; }
+    public T NewValue { get; set; }
+    public T OldValue { get; set; }
+
+    public XElement[] ToXML()
+    {
+      return new XElement[]{
+        new XElement("User", User),
+        new XElement("Project", Project),
+        new XElement("Date", Date.ToShortDateString()),
+        new XElement("OldValue", OldValue.ToString()),
+        new XElement("NewValue", NewValue.ToString())};
+    }
+  }
+}
---
+++ trunk/HydroNumerics/JupiterTools/JupiterPlus/ChangeWriter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+using HydroNumerics.JupiterTools;
+
+
+namespace HydroNumerics.JupiterTools.JupiterPlus
+{
+  public class ChangeWriter
+  {
+    public XElement WellX(string WellID, Change<double> change)
+    {
+      XElement c = BoreHoleChange(WellID);
+      c.Add(new XElement("Property", "XUTM"),
+        change.ToXML());
+      return c;
+    }
+
+    public XElement WellY(string WellID, Change<double> change)
+    {
+      XElement c = BoreHoleChange(WellID);
+      c.Add(new XElement("Property", "YUTM"),
+        change.ToXML());
+      return c;
+    }
+
+    public XElement WellTerrain(string WellID, Change<double> change)
+    {
+      XElement c = BoreHoleChange(WellID);
+      c.Add(new XElement("Property", "ELEVATION"),
+        change.ToXML());
+      return c;
+    }
+
+    private XElement BoreHoleChange(string WellID)
+    {
+      XElement c = Change();
+      c.Add(new XElement("Table", "BOREHOLE"),
+       new XElement("IdentifierKeys", new XElement("Key", WellID)));
+       return c;
+    }
+
+    private XElement Change()
+    {
+      return new XElement("Change");
+    }
+
+
+  }
+}
