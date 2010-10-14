590f825dc0323528d88afb3af2cdb1d52d4a78a9
jacobgudbjerg@gmail.com <jacobgudbjerg@gmail.com@9a3f36a8-2785-11df-8e46-3928208ddfa0>
2010-10-14 14:20:24 +0000

    Gjeller sø startet

---
+++ trunk/HydroNumerics/HydroNet/Core/UnitTest/HydroNumerics.HydroNet.Core.UnitTest/LakeGjeller.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using HydroNumerics.Time.Core;
+
+namespace HydroNumerics.HydroNet.Core.UnitTest
+{
+  /// <summary>
+  /// Summary description for LakeGjeller
+  /// </summary>
+  [TestClass]
+  public class LakeGjeller
+  {
+    public LakeGjeller()
+    {
+      //
+      // TODO: Add constructor logic here
+      //
+    }
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    //
+    // You can use the following additional attributes as you write your tests:
+    //
+    // Use ClassInitialize to run code before running the first test in the class
+    // [ClassInitialize()]
+    // public static void MyClassInitialize(TestContext testContext) { }
+    //
+    // Use ClassCleanup to run code after all tests in a class have run
+    // [ClassCleanup()]
+    // public static void MyClassCleanup() { }
+    //
+    // Use TestInitialize to run code before running each test 
+    // [TestInitialize()]
+    // public void MyTestInitialize() { }
+    //
+    // Use TestCleanup to run code after each test has run
+    // [TestCleanup()]
+    // public void MyTestCleanup() { }
+    //
+    #endregion
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+      Lake Gjeller = LakeFactory.GetLake("Gjeller Sø");
+      Gjeller.Depth = 1.2;
+      Gjeller.WaterLevel = 0.4;
+
+
+      
+      
+     
+     
+      
+
+
+
+    }
+  }
+}
