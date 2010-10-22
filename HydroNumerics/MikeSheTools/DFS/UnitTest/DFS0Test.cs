04dd143dd60a669ac695276a61b277a1b65aab72
jacobgudbjerg <jacobgudbjerg@gmail.com>
2010-10-22 14:13:49 +0000

    DFS tests no longer based on NUnit

--- HydroNumerics/MikeSheTools/DFS/UnitTest/DFS0Test.cs
+++ HydroNumerics/MikeSheTools/DFS/UnitTest/DFS0Test.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
 namespace HydroNumerics.MikeSheTools.DFS.UnitTest
 {
-  [TestFixture]
+  [TestClass()]
   public class DFS0Test
   {
 
 
-    [Test]
+    [TestMethod]
     public void ReadDataTest()
     {
-      DFS0 _dfs0 = new DFS0(@"..\..\TestData\novomr4_indv_dfs0_ud1.dfs0");
+      DFS0 _dfs0 = new DFS0(@"..\..\..\TestData\novomr4_indv_dfs0_ud1.dfs0");
       Assert.AreEqual(33316.7, _dfs0.GetData(0, 1), 1e-1);
       _dfs0.Dispose();
     }
 
-    [Test]
+    [TestMethod]
     public void ReadItems()
     {
-      DFS0 _data = new DFS0(@"..\..\\TestData\Detailed timeseries output.dfs0");
+      DFS0 _data = new DFS0(@"..\..\..\TestData\Detailed timeseries output.dfs0");
 
       Assert.AreEqual(3, _data.GetTimeStep(new DateTime(2000, 1, 4, 11, 0, 0)));
       Assert.AreEqual(3, _data.GetTimeStep(new DateTime(2000, 1, 4, 12, 0, 0)));
