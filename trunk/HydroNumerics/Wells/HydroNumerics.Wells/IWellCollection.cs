6ef540b6fb771ed45802b355236186d8046db5c4
jacobgudbjerg@gmail.com <jacobgudbjerg@gmail.com@9a3f36a8-2785-11df-8e46-3928208ddfa0>
2010-11-24 15:22:39 +0000

    New well collection

---
+++ trunk/HydroNumerics/Wells/HydroNumerics.Wells/IWellCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace HydroNumerics.Wells
+{
+  public class IWellCollection:KeyedCollection<string,IWell>
+  {
+    public IWellCollection() : base() { }
+
+    protected override string GetKeyForItem(IWell item)
+    {
+      return item.ID;
+    }
+
+  }
+}
