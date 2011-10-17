49bd6f70f520428af27304796eba3b2b631c7ad0
jacobgudbjerg@gmail.com <jacobgudbjerg@gmail.com@9a3f36a8-2785-11df-8e46-3928208ddfa0>
2011-10-17 23:10:58 +0000

    closer to OpenMi2 compatible

--- trunk/HydroNumerics/Time/OpenMI2/LinkableTimeSeriesGroup.cs
+++ trunk/HydroNumerics/Time/OpenMI2/LinkableTimeSeriesGroup.cs
@@ -3,70 +3,122 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using OpenMI.Standard2;
+using OpenMI.Standard2.TimeSpace;
 using HydroNumerics.OpenMI.Sdk.Backbone;
 using HydroNumerics.Time.Core;
 
 
-
-
 namespace HydroNumerics.Time.OpenMI2
 {
-    public class LinkableTimeSeriesGroup : LinkableComponent
+  public class LinkableTimeSeriesGroup : LinkableComponent
+  {
+    TimeSeriesGroup timeSeriesGroup;
+    string filename;
+    string outputFilename;
+
+    
+
+    public override IList<IBaseInput> Inputs { get;  protected set; }
+
+    public override IList<IBaseOutput> Outputs { get; protected set; }
+
+    public override List<IAdaptedOutputFactory> AdaptedOutputFactories { get; protected set; }
+
+    public override void Initialize()
     {
-        TimeSeriesGroup timeSeriesGroup;
-        string filename;
-        string outputFilename;
 
-        public override IList<IBaseInput> Inputs
+      Dictionary<string, string> argumentsDictionary = new Dictionary<string, string>();
+
+      //TODO: Check that you can assume that e.g. a configuration editor will populate the Argumenst, based on data from the OMI file
+      foreach (IArgument argument in this.Arguments)
+      {
+        argumentsDictionary.Add(argument.Id, argument.ValueAsString);
+      }
+      filename = argumentsDictionary["Filename"];
+      //outputFilename = argumentsDictionary["OutputFilename"];
+
+      timeSeriesGroup = TimeSeriesGroupFactory.Create(filename);
+
+      Outputs = new List<IBaseOutput>();
+
+
+      foreach (var ts in timeSeriesGroup.Items)
+      {
+        Dimension dimention = new Dimension();
+        dimention.SetPower(DimensionBase.AmountOfSubstance, ts.Unit.Dimension.AmountOfSubstance);
+        dimention.SetPower(DimensionBase.Currency, ts.Unit.Dimension.Currency);
+        dimention.SetPower(DimensionBase.ElectricCurrent, ts.Unit.Dimension.ElectricCurrent);
+        dimention.SetPower(DimensionBase.Length, ts.Unit.Dimension.Length);
+        dimention.SetPower(DimensionBase.LuminousIntensity, ts.Unit.Dimension.LuminousIntensity);
+        dimention.SetPower(DimensionBase.Mass, ts.Unit.Dimension.Mass);
+        dimention.SetPower(DimensionBase.Time, ts.Unit.Dimension.Time);
+
+        Unit unit = new Unit(ts.Unit.ID);
+        unit.Description = ts.Unit.Description;
+        unit.ConversionFactorToSI = ts.Unit.ConversionFactorToSI;
+        unit.OffSetToSI = ts.Unit.OffSetToSI;
+        unit.Dimension = dimention;
+
+        Quantity quantity = new Quantity();
+        quantity.Caption = ts.Name;
+        quantity.Description = ts.Description;
+        quantity.Unit = unit;
+
+        ElementSet elementSet = new ElementSet("IDBased");
+        elementSet.Description = "IDBased";
+        elementSet.ElementType = global::OpenMI.Standard2.TimeSpace.ElementType.IdBased;
+        Element element = new Element();
+        element.Caption = "IDBased";
+        elementSet.AddElement(element);
+
+
+
+        Output o = new Output(ts.Name, quantity, elementSet);
+
+        o.TimeSet = new TimeSet();
+        o.Values = new HydroNumerics.OpenMI.Sdk.Backbone.Generic.TimeSpaceValueSet<double>();
+
+        if (ts is TimespanSeries)
         {
-            get { throw new NotImplementedException(); }
+          foreach (var tsi in ((TimespanSeries)ts).Items)
+          {
+            o.TimeSet.Times.Add(new HydroNumerics.OpenMI.Sdk.Backbone.Time(ts.StartTime, ts.EndTime));
+            o.Values.Values2D.Add(new List<double> { tsi.Value });
+          }
+        }
+        else
+        {
+          foreach (var tsi in ((TimestampSeries)ts).Items)
+          {
+            o.TimeSet.Times.Add(new HydroNumerics.OpenMI.Sdk.Backbone.Time(ts.StartTime));
+            o.Values.Values2D.Add(new List<double> { tsi.Value });
+          }
         }
 
-        public override IList<IBaseOutput> Outputs
-        {
-            get { throw new NotImplementedException(); }
-        }
+        Outputs.Add(o);
 
-        public override List<IAdaptedOutputFactory> AdaptedOutputFactories
-        {
-            get { throw new NotImplementedException(); }
-        }
+      }
 
-        public override void Initialize()
-        {
-            Dictionary<string, string> argumentsDictionary = new Dictionary<string, string>();
-            
-            //TODO: Check that you can assume that e.g. a configuration editor will populate the Argumenst, based on data from the OMI file
-            foreach (IArgument argument in this.Arguments)
-            {
-                argumentsDictionary.Add(argument.Id, argument.ValueAsString);
-            }
-            filename = argumentsDictionary["Filename"];
-            outputFilename = argumentsDictionary["OutputFilename"];
-
-            timeSeriesGroup = TimeSeriesGroupFactory.Create(filename);
-
-            
-        }
-
-        public override string[] Validate()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Prepare()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Update(params IBaseOutput[] requiredOutput)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Finish()
-        {
-            throw new NotImplementedException();
-        }
     }
+
+
+    public override string[] Validate()
+    {
+      return new string[] { }; 
+    }
+
+    public override void Prepare()
+    {
+      
+    }
+
+    public override void Update(params IBaseOutput[] requiredOutput)
+    {
+      
+    }
+
+    public override void Finish()
+    {
+    }
+  }
 }
