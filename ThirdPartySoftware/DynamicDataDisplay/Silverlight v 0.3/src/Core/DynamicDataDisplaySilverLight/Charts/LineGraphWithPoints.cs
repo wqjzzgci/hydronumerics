ce6270d4864cee1e4d50e089bde8114bcca22cba
jacobgudbjerg <jacobgudbjerg@gmail.com>
2012-08-24 14:51:40 +0000

    Now supports itemssource and itemstemplate

--- ThirdPartySoftware/DynamicDataDisplay/Silverlight v 0.3/src/Core/DynamicDataDisplaySilverLight/Charts/LineGraphWithPoints.cs
+++ ThirdPartySoftware/DynamicDataDisplay/Silverlight v 0.3/src/Core/DynamicDataDisplaySilverLight/Charts/LineGraphWithPoints.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Net;
+using System.Collections;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,18 +21,22 @@ namespace Microsoft.Research.DynamicDataDisplay
 {
   public class LineGraphWithPoints : LineGraph
   {
-    private List<Shape> Markers = new List<Shape>();
-
-    private bool firsttime = true;
+    private List<FrameworkElement> Markers = new List<FrameworkElement>();
 
 
     public LineGraphWithPoints()
       : base()
     {
+      points = new List<Point>();
+//      DataSource = new Microsoft.Research.DynamicDataDisplay.DataSources.EnumerableDataSource<Point>(points);
+
       MarkerHeight = 10;
       MarkerWidth = 10;
       ShowInPlotter = true;
       ShowMarker = true;
+
+      
+      
     }
 
     public LineGraphWithPoints(IPointDataSource pointSource, string description)
@@ -109,100 +116,172 @@ namespace Microsoft.Research.DynamicDataDisplay
     private static void OnShowInPlotterPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
       LineGraphWithPoints source = d as LineGraphWithPoints;
-      source.ClearGraph();
+      source.UpdateCore();
+      if (source.Viewport != null)
+        source.Viewport.FitToView();
+
     }
 
 
-    public void ClearGraph()
+    public override void OnPlotterAttached(Plotter plotter)
     {
-      
-      if (ShowInPlotter)
-      {
-        UpdateCore();
-        if (Viewport != null)
-          Viewport.FitToView();
-
-      }
-      else
-      {
-        foreach (var v in Markers)
-          Plotter.MainCanvas.Children.Remove(v);
-        Markers.Clear();
-        Plotter.MainCanvas.Children.Remove(path);
-        if (Viewport != null)
-          Viewport.FitToView();
-
-      }
+      DataContext = plotter.DataContext;
+      base.OnPlotterAttached(plotter);
     }
 
+    List<Point> points;
 
     protected override void UpdateCore()
     {
-      if (DataSource == null || Viewport == null || Viewport.Output == new Rect(0, 0, 0, 0) || !ShowInPlotter) return;
+
+      if (ItemsSource == null || !ItemsSource.GetEnumerator().MoveNext() || Viewport == null || Viewport.Output == new Rect(0, 0, 0, 0)) return;
 
       if (path.Clip == null) updateClippingRect();
 
+      segments.Clear();
+      foreach (var m in Markers)
+        Plotter.MainCanvas.Children.Remove(m);
 
-      if (!Plotter.MainCanvas.Children.Contains(path))
-        Plotter.MainCanvas.Children.Add(path);
-
-      Rect output = Viewport.Output;
-      var transform = GetTransform();
-
-      IEnumerable<Point> points = GetPoints();
-
-      ContentBounds = BoundsHelper.GetViewportBounds(points, transform.DataTransform);
-
-      List<Point> transformedPoints = transform.DataToScreen(points);
-      filteredPoints = new FakePointList(FilterPoints(transformedPoints),
-          output.Left, output.Right, output.Top, output.Bottom);
-      if (filteredPoints.Count != 0)
+      if (ShowInPlotter)
       {
-        segments.Clear();
+        if (!Plotter.MainCanvas.Children.Contains(path))
+          Plotter.MainCanvas.Children.Add(path);
 
-        foreach (var v in Markers)
-          Plotter.MainCanvas.Children.Remove(v);
-        if (firsttime | filteredPoints.Count != Markers.Count)
-          Markers.Clear();
+        var transform = GetTransform();
+        var transformedpoints = transform.DataToScreen(points);
 
-        for (int i = 0; i < filteredPoints.Count; i++)
+        figure.StartPoint = transformedpoints[0];
+        for (int i = 0; i < transformedpoints.Count; i++)
         {
           LineSegment segment = new LineSegment();
-          segment.Point = filteredPoints[i];
+          segment.Point = transformedpoints[i];
           segments.Add(segment);
 
           //Update the points
           if (ShowMarker)
           {
-            if (firsttime | filteredPoints.Count != Markers.Count)
-            {
-              Ellipse e = new Ellipse();
-              e.Width = MarkerWidth;
-              e.Height = MarkerHeight;
-              e.Fill = new SolidColorBrush(this.LineColor);
-              e.Opacity = 0.65;
-              Markers.Add(e);
-              firsttime = false;
-              
-            }
-
-            double canvasy = filteredPoints[i].Y - Markers[i].Height / 2;
-            double canvasx = filteredPoints[i].X - Markers[i].Width / 2;
+            double canvasy = transformedpoints[i].Y - Markers[i].Height / 2;
+            double canvasx = transformedpoints[i].X - Markers[i].Width / 2;
+            Canvas.SetTop(Markers[i], canvasy);
+            Canvas.SetLeft(Markers[i], canvasx);
 
             if (canvasy < Plotter.MainCanvas.ActualHeight & canvasy > 0 & canvasx < Plotter.MainCanvas.ActualWidth & canvasx > 0)
             {
-              Canvas.SetTop(Markers[i], canvasy);
-              Canvas.SetLeft(Markers[i], canvasx);
               Plotter.MainCanvas.Children.Add(Markers[i]);
             }
           }
         }
-        figure.StartPoint = filteredPoints.StartPoint;
+        ContentBounds = BoundsHelper.GetViewportBounds(points, transform.DataTransform);
+        figure.Segments = segments;
+      }
+      else
+      {
+        if (Plotter.MainCanvas.Children.Contains(path))
+          Plotter.MainCanvas.Children.Remove(path);
       }
     }
 
+    public IEnumerable ItemsSource
+    {
+      get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+      set {
+        SetValue(ItemsSourceProperty, value);
+      }
+    }
+
+    public DataTemplate ItemTemplate
+    {
+      get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+      set { SetValue(ItemTemplateProperty, value); }
+    }
+
+    public static readonly DependencyProperty ItemsSourceProperty =
+DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(LineGraphWithPoints),
+    new PropertyMetadata((s, e) => ((LineGraphWithPoints)s).UpdateItems()));
+
+    public static readonly DependencyProperty ItemTemplateProperty =
+    DependencyProperty.Register("ItemTemplate", typeof(DataTemplate), typeof(LineGraphWithPoints),
+    new PropertyMetadata((s, e) => ((LineGraphWithPoints)s).UpdateItems()));
+
+    HorizontalDateTimeAxis HorAxis;
+
+
+    private void UpdateItems()
+    {
+      if (ItemsSource != null & Plotter !=null)
+      {
+        HorAxis = (Plotter as ChartPlotter).HorizontalAxis as HorizontalDateTimeAxis;
+
+
+        points = new List<Point>();
+        foreach (var m in Markers)
+          Plotter.MainCanvas.Children.Remove(m);
+        Markers.Clear();
+
+        foreach (var p in ItemsSource)
+        {
+          var visualItem = this.ItemTemplate.LoadContent() as FrameworkElement;
+          visualItem.DataContext = p;
+          double v = LineGraphWithPoints.GetYValue(visualItem as DependencyObject);
+          DateTime d = LineGraphWithPoints.GetDateValue(visualItem as DependencyObject);
+          Point pdata = new Point(HorAxis.ConvertToDouble(d), v);
+          points.Add(pdata);
+          Markers.Add(visualItem);
+        }
+        var es = new Microsoft.Research.DynamicDataDisplay.DataSources.EnumerableDataSource<Point>(points);
+        es.SetXMapping(p => p.X);
+        es.SetYMapping(p => p.Y);
+
+        DataSource = es;
+
+      }
+
+      if (Viewport != null)
+        Viewport.FitToView();
+
+    }
+
+    
+
+
+
+     public static readonly DependencyProperty YValueProperty = DependencyProperty.RegisterAttached(
+                         "YValue",                  //Name of the property
+                         typeof( double ),             //Type of the property
+                         typeof( LineGraphWithPoints ),   //Type of the provider of the registered attached property
+                         null );                           //Callback invoked in case the property value has changed
+ 
+ 
+                public static void SetYValue( DependencyObject obj, double yValue )
+                {
+                         obj.SetValue( YValueProperty, yValue );
+                }
+ 
+                public static double GetYValue( DependencyObject obj )
+                {
+                         return ( double )obj.GetValue( YValueProperty );
+                }
+
 
    
+         public static readonly DependencyProperty DateValueProperty = DependencyProperty.RegisterAttached(
+                         "DateValue",                  //Name of the property
+                         typeof( DateTime ),             //Type of the property
+                         typeof( LineGraphWithPoints ),   //Type of the provider of the registered attached property
+                         null );                           //Callback invoked in case the property value has changed
+ 
+ 
+                public static void SetDateValue( DependencyObject obj, DateTime yValue )
+                {
+                         obj.SetValue( DateValueProperty, yValue );
+                }
+ 
+                public static DateTime GetDateValue( DependencyObject obj )
+                {
+                         return ( DateTime )obj.GetValue( DateValueProperty );
+                }
+
+
 
 
     public bool ShowMarker { get; set; }
